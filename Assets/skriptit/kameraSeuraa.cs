b50386d6ae43ac223a32bcf1e277f4494de20458
coldase <jarzu@pm.me>
2021-01-12 15:43:29 +0200

    added car movement

---
+++ Assets/skriptit/kameraSeuraa.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kameraSeuraa : MonoBehaviour
+{
+    public Transform target;
+    public float PosLerp = 0.2f;
+    public float RotLerp = 0.25f;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, PosLerp);
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, RotLerp);
+
+    }
+
+}
